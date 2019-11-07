d4e77f5299c285e912e91ddfc268e6dfdc40308d
Thomas von Deyen <thomas@vondeyen.com>
2019-11-07 08:52:30 +0100

    Use menus instead of navigation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-require 'dotenv/load'
-require 'demo/alchemy_picture_seeder'
+require "dotenv/load"
+require "demo/alchemy_picture_seeder"
 
 # Pictures
 Demo::AlchemyPictureSeeder.seed!
@@ -14,40 +14,82 @@ Alchemy::Seeder.seed!
   image_teaser: 7,
   blog_post: 11,
   category_intro: 13,
-  content_block: 14
+  content_block: 14,
 }.each do |element_name, picture_id|
   Alchemy::EssencePicture.joins(:element)
-    .where(alchemy_elements: {name: element_name})
+    .where(alchemy_elements: { name: element_name })
     .update_all(picture_id: picture_id)
 end
 
 # Video assigments
 Alchemy::EssenceText.joins(:element)
-  .where(alchemy_elements: {name: 'video_slide'})
-  .update_all(body: '225795837')
+  .where(alchemy_elements: { name: "video_slide" })
+  .update_all(body: "225795837")
 
-Alchemy::EssenceText.joins(:element)
-  .where(alchemy_elements: {name: 'footer_links_link'})
-  .update_all(body: 'Admin Login', link: '/admin')
+puts "\nSeeding Users"
+puts "-------------"
 
 # Admin user
-if ENV['ADMIN_LOGIN']
+if ENV["ADMIN_LOGIN"]
   Alchemy.user_class.create!(
-    login: ENV['ADMIN_LOGIN'],
-    email: ENV['ADMIN_EMAIL'],
-    password: ENV['ADMIN_PASSWORD'],
-    password_confirmation: ENV['ADMIN_PASSWORD'],
-    alchemy_roles: %w(admin)
+    login: ENV["ADMIN_LOGIN"],
+    email: ENV["ADMIN_EMAIL"],
+    password: ENV["ADMIN_PASSWORD"],
+    password_confirmation: ENV["ADMIN_PASSWORD"],
+    alchemy_roles: %w(admin),
   )
   puts "== Created admin user"
 end
 
 # Demo user
 Alchemy.user_class.create!(
-  login: 'demo',
-  email: 'demo@example.com',
-  password: 'demo123',
-  password_confirmation: 'demo123',
-  alchemy_roles: %w(demo)
+  login: "demo",
+  email: "demo@example.com",
+  password: "demo123",
+  password_confirmation: "demo123",
+  alchemy_roles: %w(demo),
 )
 puts "== Created demo user"
+
+# Menus
+
+puts "\nSeeding Menus"
+puts "-------------"
+
+YAML.load_file("./db/seeds/alchemy/pages.yml").each do |page|
+  if page["layoutpage"]
+    name = "footer_navigation"
+  else
+    name = "main_navigation"
+  end
+
+  parent_page = Alchemy::Page.find_by!(name: page["name"], page_layout: page["page_layout"])
+  parent = Alchemy::Node.create!(
+    name: name,
+    site: parent_page.site,
+    language: parent_page.language,
+  )
+  puts "== Created menu: #{parent.name}"
+
+  if page["children"]
+    page["children"].each do |child|
+      child_page = Alchemy::Page.find_by!(name: child["name"], page_layout: child["page_layout"])
+      Alchemy::Node.create!(
+        parent: parent,
+        page: child_page,
+        site: parent.site,
+        language: parent.language,
+      )
+      puts "== Created menu entry: #{child_page.name}"
+    end
+  else
+    Alchemy::Node.create!(
+      parent: parent,
+      url: "/admin",
+      name: "Admin Login",
+      site: parent.site,
+      language: parent.language,
+    )
+    puts "== Created menu entry: Admin Login"
+  end
+end
