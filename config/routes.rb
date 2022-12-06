4ac7e77deeaa22664c4ff1cbdf6992f34e434147
Thomas von Deyen <thomas@vondeyen.com>
2022-12-06 17:14:43 +0100

    Mount health check

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  mount HealthBit.rack => "/healthz"
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  mount Alchemy::GraphQL::Engine => '/'
-  mount Alchemy::Engine => '/'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Alchemy::GraphQL::Engine => "/"
+  mount Alchemy::Engine => "/"
 end
