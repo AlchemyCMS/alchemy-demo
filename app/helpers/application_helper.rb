0326c57ea385b4f2974212b078fb9d5d99f94fac
Thomas von Deyen <thomas@vondeyen.com>
2022-12-06 21:47:10 +0100

    Remove usage of elements.available
    This has been removed in Alchemy 6.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,18 @@
 module ApplicationHelper
-
   def microsite
-    Alchemy::Page.published.where(page_layout: 'microsite').first
+    Alchemy::Page.published.where(page_layout: "microsite").first
   end
 
   def blog_page(page)
-    page.children.published.where(page_layout: 'blog').first
+    page.children.published.where(page_layout: "blog").first
   end
 
   def blog_articles(page)
     blog_page = blog_page(page)
-    if blog_page
-      blog_page.elements.available.named(:blog_post)
-    end
+    blog_page&.elements&.named(:blog_post)
   end
 
   def footer_page
-    @footer_page ||= Alchemy::Page.where(page_layout: 'footer').first
+    @footer_page ||= Alchemy::Page.where(page_layout: "footer").first
   end
-
 end
