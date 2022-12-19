445a01567cf02ab66d022624c299e14a76f3521a
Thomas von Deyen <thomas@vondeyen.com>
2022-12-19 09:27:22 +0100

    Update to Rails 6.1

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem "alchemy_i18n", "~> 3.0"
 gem "alchemy_cloudinary", "~> 2.0"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem "rails", "~> 6.0.0"
+gem "rails", "~> 6.1.0"
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.0"
 # Use Puma as the app server
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,8 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
-
-Raven.configure do |config|
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-end
+Rails.application.config.filter_parameters += [
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+]
