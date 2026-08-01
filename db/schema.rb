d008cc67cecd076c41a12a1b4ea4709edc6c5886
Thomas von Deyen <thomas@vondeyen.com>
2026-08-01 13:28:52 +0200

    Update rails to 8.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_08_01_074120) do
+ActiveRecord::Schema[8.0].define(version: 2026_08_01_110819) do
   create_table "alchemy_attachments", force: :cascade do |t|
     t.string "name"
     t.string "file_name"
